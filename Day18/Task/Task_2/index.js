44d5fe40de79598057e91fcbd64907b2797937f2
Touhidul Haque Sifat <touhidul01752@gmail.com>
2025-08-16 23:25:37 +0600

    add Task 2

---
+++ Day18/Task/Task_2/index.js
@@ -0,0 +1,31 @@
+const nameElm = document.getElementById("nameInput");
+const ageElm = document.getElementById("ageInput");
+const roleElm = document.getElementById("roleInput");
+
+function addRow(){
+    let tableElm = document.getElementById("myTable").getElementsByTagName('tbody')[0];
+    newRow = tableElm.insertRow();
+
+    const nameCell = newRow.insertCell(0);
+    const ageCell = newRow.insertCell(1);
+    const roleCell = newRow.insertCell(2);
+    const actionCell = newRow.insertCell(3);
+    nameCell.textContent = nameElm.value;
+    ageCell.textContent = ageElm.value;
+    roleCell.textContent = roleElm.value;
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete";
+    deleteBtn.className = "delete-btn";
+    deleteBtn.onclick = function() {
+        const row = this.parentElement.parentElement;
+        row.parentElement.removeChild(row);
+    };
+    actionCell.appendChild(deleteBtn);
+
+   
+    document.getElementById("nameInput").value = "";
+    document.getElementById("ageInput").value = "";
+    document.getElementById("roleInput").value = "";
+
+}
